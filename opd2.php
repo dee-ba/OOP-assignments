5156d62479e5df2585119160e295fbad52ca4e22
dee <info.dibaalizai@gmail.com>
2022-04-26 10:41:47 +0200

    commit

--- opd2.php
+++ opd2.php
@@ -9,63 +9,26 @@
 <body>
 <?php
 
-echo "Opdracht 2
-Write a simple PHP class which displays an introductory message like "Hello All, I am Scott", where "Scott" is an argument value of the method within the class." 
+// "Opdracht 2
+// Write a simple PHP class which displays an introductory message like "Hello All, I am Scott", where "Scott" is an argument value of the method within the class." 
+
 // Solution 1
 class Message {
-
   // Properties
-  public $name;
-
-
+  public $greet;
   // Methods
-  function set_name($name) {
-    $this->name = $name;
+  function set_name($greet) {
+    $this->greet = $greet;
   }
   function get_name() {
-    return $this->name;
+    return $this->greet;
   }
 
 }
 
-$user_greeting = new Message ();
-$user_greeting -> new Message ('hello');
-
-
-
-
-
-
-
-
-
-
-
-
-//Solution 2
-// class Message 
-//   // Properties
-//   public $greet;
-//   // Methods
-//   function set_name($greet) {
-//     $this->greet = $greet;
-//   }
-//   function get_name() {
-//     return $this->greet;
-//   }
-
-// }
-
-// $scott = new Message();
-// $scott->set_name('Scott');
-// echo "Hello All, I am " . $scott->get_name();
-
-
-
-
-
-
-
+$scott = new Message();
+$scott->set_name('Scott');
+echo "Hello All, I am " . $scott->get_name();
 
 
 ?>
