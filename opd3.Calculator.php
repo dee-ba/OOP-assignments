39b9e9dd7d3c8153f2ccf83b3eae105bb6362b26
dee <info.dibaalizai@gmail.com>
2022-04-26 21:03:02 +0200

    commit

--- opd3.Calculator.php
+++ opd3.Calculator.php
@@ -17,41 +17,41 @@ echo $mycalc- > multiply(); // Displays 72 -->
 
 <?php
    class Calculator {
-    private $_val1 , $_val2;
+    private $num1 , $num2;
 
-    public function __construct($val1, $val2){
-        $this->_val1 = $val1;
-        $this->_val2 = $val2;
+    public function __construct($num1, $num2){
+        $this->num1 = $num1;
+        $this->num2 = $num2;
     }
 
     public function add(){
-        return $this->_val1 + $this->_val2;
+        return $this->num1 + $this->num2;
     }
 
     public function subtract(){
-        return $this->_val1 - $this->_val2;
+        return $this->num1 - $this->num2;
     }
 
     public function multiply (){
-        return $this->_val1 * $this->_val2;
+        return $this->num1 * $this->num2;
     }
 
     public function divide () {
-        return $this->_val1 / $this->_val2;
+        return $this->num1 / $this->num2;
     }
 }
 
-$calc = new Calculator(3,4);
-echo "<p>3 + 4 = ".$calc->add(). "</p>";
+$mycalc = new Calculator(12,6);
+echo "<p> 12 + 6 = ". $mycalc->add(). "</p>"; 
 
-$calc = new Calculator (15,12);
-echo "<p>15 - 12 = ".$calc->subtract(). "</p>";
+$mycalc = new Calculator (15,12);
+echo "<p> 15 - 12 = ".$mycalc->subtract(). "</p>";
 
-$calc = new Calculator (20,2);
-echo "<p> 20 * 2 = ".$calc->multiply(). "</p>";
+$mycalc = new Calculator (20,2);
+echo "<p> 20 * 2 = ".$mycalc->multiply(). "</p>";
 
-$calc = new Calculator (20,2);
-echo "<p> 20 / 2 = ".$calc ->divide(). "</p>";
+$mycalc = new Calculator (20,2);
+echo "<p> 20 / 2 = ".$mycalc ->divide(). "</p>";
 
 
 ?>
