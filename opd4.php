8a4d3e1900922560d18f58a6cd33726b2a9474a0
dee <info.dibaalizai@gmail.com>
2022-04-26 12:03:59 +0200

    commit

--- opd4.php
+++ opd4.php
@@ -7,6 +7,24 @@
     <title>Document</title>
 </head>
 <body>
+
+<!-- Oefen Eindopdrachten 
+Opdracht 1 - Taxikosten
+Een taxibedrijf heeft een aantal taxi‟s. De volgende tarieven worden gehanteerd:
+
+Per gereden km € 0,50. 
+Daarboven per gereden minuut € 0,17. 
+Van vrijdagavond 22.00 uur tot maandagochtend 7.00 uur geldt een toeslag van 15% (het begintijdstip is hiervoor bepalend).
+
+Er dient een programma te worden ontwikkeld waarmee voor iedere taxirit de begin-en eindtijd (formaat hhmm), evenals de weekdag en de afstand kunnen worden ingevoerd. Voor elke rit moet het verschuldigde bedrag worden berekenden getoond. 
+
+Verder moeten getoond kunnen worden:
+
+
+cumulatief de totale inkomsten van het taxibedrijf.
+De gemiddelde afstanden over alle gereden ritten. 
+De langste ritafstand. -->
+
    <?php
    
    
